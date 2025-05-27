40526944dced69074686c49b6fdb15927b3172d7
Alireza Zamani <alirezazamanidev80@gmail.com>
2025-05-27 16:31:57 +0330

    chore(gateway0: add upload module

--- gateway-api/src/app.module.ts
+++ gateway-api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { appExternalImports } from './app/imports/external.imports';
 import { appInternalImports } from './app/imports/internal.imports';
+import { UploadModule } from './modules/upload/upload.module';
 
 @Module({
   imports: [
     ...appExternalImports,
     ...appInternalImports,
+    UploadModule,
 
   ],
   
