21ee1b1311e7b31b7ff5a0626baa9109959bcc43
Alireza Zamani <alirezazamanidev80@gmail.com>
2025-05-27 20:00:35 +0330

    feat: add generatePresignedUrlForFile method

--- file-service/src/services/file.service.ts
+++ file-service/src/services/file.service.ts
@@ -70,6 +70,34 @@ export class FileService implements OnModuleInit {
       });
     }
   }
+  async generatePresignedUrlForFile(
+    user: { email: string },
+    filePathInMinio: string,
+    expirySeconds?: number,
+  ) {
+    const safeEmail = user.email.replace(/@/g, '_at_').replace(/\./g, '_dot_');
+    if (!filePathInMinio.startsWith(`${safeEmail}/`))
+      throw new RpcException({
+        status: HttpStatus.FORBIDDEN,
+        message: 'Access Denied',
+      });
+    const expiry = expirySeconds || parseInt(process.env.MINIO_FILE_EXPIRES, 10);
+    try {
+        const presignedUrl=await this.minioCleint.presignedGetObject(
+            process.env.MINIO_BUCKET_NAME,
+            filePathInMinio,
+            expiry
+        )
+              return { url: presignedUrl, expires_in_seconds: expiry };
+    } catch (error) {
+            throw new RpcException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Failed to generate file access URL.',
+        details: error.message,
+      });
+    }
+   
+  }
   async getUserFiles(user: { email: string }) {
     const safeEmail = user.email.replace(/@/g, '_at_').replace(/\./g, '_dot_');
     const files: any[] = [];
@@ -82,18 +110,18 @@ export class FileService implements OnModuleInit {
     for await (const obj of stream as AsyncIterable<BucketItem>) {
       if (obj.name && !obj.name?.endsWith('/')) {
         try {
-          const expiry = parseInt(process.env.MINIO_FILE_EXPIRES,10)
-          
+          const expiry = parseInt(process.env.MINIO_FILE_EXPIRES, 10);
+
           const presignedUrl = await this.minioCleint.presignedGetObject(
             process.env.MINIO_BUCKET_NAME,
             obj.name,
-            expiry
+            expiry,
           );
-        
+
           files.push({
             filename: obj.name.substring(prifix.length),
             expiresAt: new Date(Date.now() + expiry * 1000).toISOString(),
-            expiresIn: `${expiry} sec`,
+            expires_in_seconds:expiry,
             url: presignedUrl,
             size: obj.size,
             lastModified: obj.lastModified,
